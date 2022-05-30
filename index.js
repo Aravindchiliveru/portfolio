463243f2ad55f4f6b8908b108645644821062ed8
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-05-30 14:01:24 +0530

    plz

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), new THREE.Shade
        }
 }))
 
+scene.add(sphere)
 
 const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), new THREE.ShaderMaterial({
     // color : 0xFF0000
@@ -47,10 +48,6 @@ const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), new THREE.S
 atmosphere.scale.set(1.1, 1.1, 1.1)
 
 scene.add(atmosphere)
-
-const group = new THREE.Group()
-//group.add(sphere)
-scene.add(group)
 
 const starGeometry = new THREE.BufferGeometry()
 const starMaterial = new THREE.PointsMaterial({
@@ -73,26 +70,10 @@ const stars = new THREE.Points(starGeometry, starMaterial)
 scene.add(stars)
 camera.position.z = 15
 
-const mouse = {
-    x : 0,
-    y : 0
-}
-
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
     sphere.rotation.y += 0.003
-    gsap.to(group.rotation, {
-        x : -mouse.y * 0.3,
-        y : mouse.x * 0.5,
-        duration : 2,
-    })
 }
 
 animate()
-
-
-addEventListener('mousemove', () => {
-    mouse.x = (event.clientX/innerWidth)*2 - 1;
-    mouse.y = (event.clientY/innerHeight)*2 + 1;
-})
