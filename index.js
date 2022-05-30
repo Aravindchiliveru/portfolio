a1fa03146e2bdca35dd20596c02b802e8bed67b2
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-05-30 06:27:51 +0530

    eh po ra

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), new THREE.Shade
        fragmentShader : fragmentShader,
        uniforms : {
            globeTexture : {
-               value : new THREE.TextureLoader().load('globe.jpg')
+               value : new THREE.TextureLoader().load('../globe.jpg')
            }
        }
 }))
