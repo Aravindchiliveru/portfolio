f1ca78b497b868f550c21af8276d5eb52df2c41d
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-06-02 03:25:57 +0530

    last

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ renderer.setPixelRatio(window.devicePixelRatio)
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), new THREE.MeshBasicMaterial({
     // color : 0xFF0000
-      map : new THREE.TextureLoader().load('globe.jpg')
+      map : new THREE.TextureLoader().load('./globe.jpg')
 }))
 
 
