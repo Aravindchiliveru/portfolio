7f269a8b8e05ee56e5b70baca5b2748ed66dd429
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-06-02 04:23:24 +0530

    n

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fragmentShader from './shaders/fragment.glsl'
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
 import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
 import gsap from 'gsap'
-import { Float32BufferAttribute } from 'three'
+import imagewrap from './globe.jpg'
 
 
 const canvasContainer = document.querySelector('#canvasContainer')
@@ -27,7 +27,7 @@ renderer.setPixelRatio(window.devicePixelRatio)
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), new THREE.MeshBasicMaterial({
     // color : 0xFF0000
-      map : new THREE.TextureLoader().load('./globe.jpg')
+      map : new THREE.TextureLoader().load(imagewrap)
 }))
 
 
